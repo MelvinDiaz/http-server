7015a65f3dc58d06d9bb9275d2c8e4aaf6bbed40
MelvinDiaz <melvin.diazayala@proton.me>
2024-09-17 14:18:14 -0600

    Socket listening to port 8080

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
+#include <complex.h>
+#include <cstdio>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
 
 void setup_socket() {
+
+  struct sockaddr_in server_address;
 
   // AF_INET is the address family for IPv4
   // SOCK_STREAM for two way connection based byte streams
@@ -11,6 +16,30 @@ void setup_socket() {
     // handle error
     perror("Error creating socket");
   }
+
+  // Config socket
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(8080);
+  // Bind to any ip address
+  server_address.sin_addr.s_addr = INADDR_ANY;
+
+  int bind_status = bind(new_socket, (struct sockaddr *)&server_address,
+                         sizeof(server_address));
+
+  if (bind_status < 0) {
+    perror("Error binding socket");
+  }
+  int listen_status = listen(new_socket, 5);
+  if (listen_status == 0) {
+    printf("Listening on port 8080\n");
+  } else {
+    perror("Error listening on port 8080");
+  }
 }
 
-int main() { return 0; }
+int main() {
+
+  setup_socket();
+
+  return 0;
+}
